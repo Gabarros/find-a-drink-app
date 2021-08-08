c5c8ce7d219bb72138d9324ccea71b44d21a4a12
Gabarros <gabarros17@gmail.com>
2021-08-08 18:28:24 -0300

    created ignore list

--- src/pages/ProductsList/ProductsList.tsx
+++ src/pages/ProductsList/ProductsList.tsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useEffect } from "react";
+
+import useProducts from "./useProducts";
+import classes from "./productsList.module.css";
 
 const ProductsList = () => {
-    return(
-        <h1>ProductsList</h1>
-    )
+  const {
+    products,
+    isLoadingProducts,
+    getProductsList,
+    getDistributorId,
+  } = useProducts();
 
+  useEffect(() => {
+    console.log("Entrou");
+    getDistributorId();
+  }, []);
+
+  return isLoadingProducts ? (
+    <div>
+      <h1>Carregando Produtos...</h1>
+    </div>
+  ) : (
+    <div>
+      <h1>Produtos carregados</h1>
+      <ul className={classes.listContainer}>
+        {products.map((product) => (
+          <li key={product?.id}>
+            <p>{product?.title}</p>
+            <img
+              src={product?.images[0].url}
+              alt={`Uma imagem de ${product?.title} `}
+              className={classes.image}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ProductsList;
