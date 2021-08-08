c5d000d91e3880f944fe9023f3e8db5437e1f980
Gabarros <gabarros17@gmail.com>
2021-08-08 18:30:36 -0300

    created services and api connection

---
+++ src/services/ApiService.ts
@@ -0,0 +1,37 @@
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+
+const uri = "https://api.code-challenge.ze.delivery/public/graphql";
+const client = new ApolloClient({
+  uri,
+  cache: new InMemoryCache(),
+});
+
+export const DISTRIBUTOR_ID = gql`
+  query pocSearchMethod(
+    $now: DateTime!
+    $algorithm: String!
+    $lat: String!
+    $long: String!
+  ) {
+    pocSearch(now: $now, algorithm: $algorithm, lat: $lat, long: $long) {
+      id
+    }
+  }
+`;
+
+export const PRODUCTS = gql`
+  query poc($id: ID!, $categoryId: Int, $search: String) {
+    poc(id: $id) {
+      id
+      products(categoryId: $categoryId, search: $search) {
+        id
+        title
+        images {
+          url
+        }
+      }
+    }
+  }
+`;
+
+export default client;
---
+++ src/services/GeolocationService.ts
@@ -0,0 +1,8 @@
+import Geocode from "react-geocode";
+
+Geocode.setApiKey("AIzaSyD4PXvkbxMluzGicYqyFhjHqqWajfIDOLI");
+Geocode.setLanguage("pt-br");
+Geocode.setRegion("pt-br");
+
+export default Geocode;
+
